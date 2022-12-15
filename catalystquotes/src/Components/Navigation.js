656e263d51104606ad58623f982833e4d3b369c9
Prashant Mishra <prashantmishra111@outlook.com>
2022-12-15 08:55:46 +0530

    nav links and search tab added !!

--- catalystquotes/src/Components/Navigation.js
+++ catalystquotes/src/Components/Navigation.js
@@ -8,10 +8,29 @@ function Navigation(props) {
         Catalyst <br />
         &nbsp; &nbsp; 🥇 Quotes
       </h1>
+      <div className="nav-links">
+        <ul>
+          <li> Home </li>
+          <li> Authors </li>
+          <li> Profile </li>
+          <li> Login/SignUp </li>
+          
+        </ul>
+      </div>
+      <div className="searchBar">
+        <input 
+        type="text"
+        placeholder="Author name"
+        />
+        <button
+        onClick={() => {}}
+        >Search</button>
+
+      </div>
 
       <h2 className="saved-container">
         <span className="saved-icon">
-          {props.value ? <span>❤️</span> : <span>🤍</span>}
+          Saved {props.value ? <span>❤️</span> : <span>🤍</span>}
         </span>
         <span className="saved-count">{props.value}</span>
       </h2>
